6f493284e4421f824e54a64a622f36002a509f27
Roman Lashkevich <rlashkevich@scislt.com>
2022-11-28 11:54:20 +0300

    Commit 2

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello 3443t43t34!");
+        System.out.println("Hello egegegerg!");
     }
 }
