b8b98d7c7bc190a04596476b25a8ed407741b766
Roman Lashkevich <rlashkevich@scislt.com>
2022-11-28 11:42:46 +0300

    Change main class

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello Vlada!");
+        System.out.println("Hello World!");
     }
 }
